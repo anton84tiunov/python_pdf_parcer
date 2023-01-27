a3de5ef518e11577ae87068018cf2af0e1c2188d
anton <anton.tiunov.84.07@gmail.com>
2023-01-27 16:44:52 +0600

    1  мое третье сохранение

--- src/Controller/pdf_editor/extract_el_from_pdf_controller.py
+++ src/Controller/pdf_editor/extract_el_from_pdf_controller.py
@@ -3,19 +3,20 @@ import src.Model.pdf_editor.extract_el_from_pdf_page as extract_el_from_pdf_page
 import src.Model.pdf_editor.convert_pdf_el_to_model as convert_pdf_el_to_model
 import src.Model.general.draw_model as drawModel
 import src.Model.general.text_model as textModel
-
+import src.Model.general.image_model as imageModel
 class ExtractElFromPdfController():
     def __init__(self):
         self.extr = extract_el_from_pdf_page.ExtractElFromPdf()
         self.conv = convert_pdf_el_to_model.ConvertPdfElToModel()
 
-    def open_pdf(self, path, num_page) -> tuple[list[drawModel.DrawModel], list[textModel.TextModel]]:
+    def open_pdf(self, path, num_page) -> tuple[list[drawModel.DrawModel], list[textModel.TextModel], list[imageModel.ImageModel]]:
         doc = self.extr.get_doc(path)
         page = self.extr.get_page(doc, num_page)
         el =  self.extr.get_el(doc, page)
         el_draw = self.conv.conv_draws(el["draw"])
         el_text = self.conv.conv_texts(el["text"])
-        return el_draw, el_text
+        el_img = self.conv.conv_images(el["image"])
+        return el_draw, el_text, el_img
 
 
 
